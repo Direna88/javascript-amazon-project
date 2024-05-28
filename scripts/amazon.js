93b8acad701b67471639bea170da750109867cc9
direna <direnabashota@gmail.com>
2024-05-28 12:47:24 +0200

    Changes to the number of quantity

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product) => {
     </div>
 
     <div class="product-quantity-container">
-      <select>
+      <select class = "js-quantity-selector-${product.id}">
         <option selected value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
@@ -69,6 +69,8 @@ document.querySelectorAll('.js-add-to-cart')
           matchingItem = item;
         }
       });
+
+        const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
 
         if(matchingItem) {
           matchingItem.quantity += 1;
